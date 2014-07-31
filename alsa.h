bccfc2ec9518442478b676b91e623135ae50c076
Eric Richter <richteer@clarkson.edu>
2014-07-31 15:28:46 -0400

    Removed frames from header

--- alsa.h
+++ alsa.h
@@ -3,6 +3,6 @@
 #endif
 
 #define ALSA_BUFFER_SIZE (ALSA_FRAMES*snd_pcm_format_width(format)/8*2)
-int init_alsa_device(char * device, int frames);
+int init_alsa_device(char * device);
 int read_alsa_data(char * buffer);
 int deinit_alsa(void);
