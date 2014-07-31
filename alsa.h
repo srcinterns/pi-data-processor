9aa0486ee6158bc346e75d0009d642de98abfe43
Eric Richter <richteer@clarkson.edu>
2014-07-31 15:33:54 -0400

    Removed weird header #define requirements

--- alsa.h
+++ alsa.h
@@ -1,7 +1,4 @@
-#ifndef ALSA_FRAMES
-#error ALSA_FRAMES not defined!
-#endif
-
+#define ALSA_FRAMES 128
 #define ALSA_BUFFER_SIZE (ALSA_FRAMES*snd_pcm_format_width(format)/8*2)
 int init_alsa_device(char * device);
 int read_alsa_data(char * buffer);
