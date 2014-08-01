3b44c28c9ee0c50226d756bdd503ecbac51b3fd0
Eric Richter <richteer@clarkson.edu>
2014-08-01 18:20:49 -0400

    Added a utility library for array conversions/etc.

---
+++ src/utility.c
@@ -0,0 +1,12 @@
+#include "utility.h"
+
+// Copies values from buffer to target (which must be size "size"), starting at the buffer offset, and incrementing every stride
+void s16_to_float_array(float * target, size_t size, int offset, int stride, uint16_t * buffer)
+{
+	int i,j;
+
+	for (i = offset; j < size; j++,i+=stride) {
+		target[j] = (float) buffer[i];
+	}
+
+}
---
+++ src/utility.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+void s16_to_float_array(float * target, size_t size, int offset, int stride, uint16_t * buffer);
