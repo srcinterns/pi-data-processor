a0c508623fb511e1e19c8ab4d38db141aed5c9ce
Eric Richter <richteer@clarkson.edu>
2014-08-01 17:57:11 -0400

    Cleaned up main.c to skeleton new version

--- src/main.c
+++ src/main.c
@@ -1,39 +1,42 @@
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 #include "alsa.h"
+#include "network.h"
+
+void sigint(int x)
+{
+	fprintf(stderr,"Received SIGINT, shutting down...\n");
+	net_close();
+	deinit_alsa();
+	exit(2);
+}
+
 
 int main(int argc, char ** argv)
 {
 
-	char buffer[1024];
-	FILE * output;
-	int size;
-	int i;
-	time_t now, end;
-
-	if (argc != 3) {
-		fprintf(stderr,"Usage: %s <device-name> <output-file>\n",argv[0]);
+	if (argc != 4) {
+		fprintf(stderr,"Usage: %s <device-name> <ip address> <port>\n",argv[0]);
 		return 1;
 	}
-	output = fopen(argv[2],"w");
 
+	signal(SIGINT, sigint);
 	init_alsa_device(argv[1]);
+	net_init(argv[2],argv[3]);
 
-	time(&end);
-	end += 10;
-	for (; now != end ; time(&now)) {
-		size = read_alsa_data(buffer);
-		if (-1 == size) break;
 
-		printf("\rRecording for %ld more seconds...                           ",end-now);
+	for(;;) {
+		// read alsa data
+		// process data
+		// send data
 
-		fwrite(buffer, 1, size, output);
 	}
-	printf("\n");
-	fclose(output);
 
 	deinit_alsa();
+	net_close();
 
 	return 0;
 }
