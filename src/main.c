f4860d0d2d04ce681ade9ad7bc938fcb6eadf524
dcampbel <dncswim76@gmail.com>
2014-08-04 19:06:24 -0400

    added messagizer files

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include "network.h"
 #include "rti.h"
 #include "radar_config.h"
+#include "messagize.h"
+
 
 #define MESSAGE_ID (0x1)
 
@@ -33,30 +35,32 @@ int main(int argc, char ** argv)
     net_init(argv[2],argv[3]);
     init_processing(); //will populate size of send_array in config_array.h
 
-    char send_data[NUM_TRIGGERS][size_of_sendarray];
-    signed short temp_buffer[2*DATA_BUFFER_SIZE];
-    float trigger[DATA_BUFFER_SIZE];
-    float response[DATA_BUFFER_SIZE];
-    unsigned int timestamp = 0;
+    char* send_data = (char*) calloc(NUM_TRIGGERS*size_of_sendarray, sizeof(char));
+    signed short* temp_buffer = (signed short*)calloc(2*DATA_BUFFER_SIZE, sizeof(signed short));
+    float *trigger = (float*) calloc(DATA_BUFFER_SIZE, sizeof(float));
+    float *response = (float*) calloc(DATA_BUFFER_SIZE, sizeof(float));
     int i;
 
     for(;;) {
 
-      read_alsa_data((char*)temp_buffer, DATA_BUFFER_SIZE);
-      s16_to_float_array(trigger, DATA_BUFFER_SIZE, 0, 1, temp_buffer);
-      s16_to_float_array(response, DATA_BUFFER_SIZE, 1, 1, temp_buffer);
-      
+        read_alsa_data((char*)temp_buffer, DATA_BUFFER_SIZE);
+        s16_to_float_array(trigger, DATA_BUFFER_SIZE, 0, 1, temp_buffer);
+        s16_to_float_array(response, DATA_BUFFER_SIZE, 1, 1, temp_buffer);
 
-      /* send_data[0][0] converts 2-d array to char*, pointing to first value*/
-      process_radar_data(&send_data[0][0], trigger,
-			 response, DATA_BUFFER_SIZE);
-      for (i = 0; i < NUM_TRIGGERS; i=i + 8){
-	net_send_data(MESSAGE_ID, timestamp, send_data[i], size_of_sendarray);
-      }
+
+        /* send_data[0][0] converts 2-d array to char*, pointing to first value*/
+        process_radar_data(&send_data[0][0], trigger,
+                           response, DATA_BUFFER_SIZE);
+        for (i = 0; i < NUM_TRIGGERS; i = i + 8){
+            messagize_and_send(&send_data[NUM_TRIGGERS*i], size_of_sendarray);
+        }
     }
 
-    
 
+    free(send_data);
+    free(temp_buffer);
+    free(trigger);
+    free(response);
     deinit_alsa();
     net_close();
     clean_up_processing();
