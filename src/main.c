3c62eba4430ebb7dec173a2a8554278224f90192
richteer <richteer@clarkson.edu>
2014-08-06 14:14:27 +0000

    Modified main loop

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,7 +10,7 @@
 #include "radar_config.h"
 #include "utility.h"
 
-#define MESSAGE_ID (0x1)
+#define POWER_CUTOFF (0)
 
 char* send_data;
 uint16_t* temp_buffer;
@@ -32,37 +33,53 @@ void sigint(int x)
 
 int main(int argc, char ** argv)
 {
-
+/*
     if (argc != 4) {
         fprintf(stderr,"Usage: %s <device-name> <ip address> <port>\n",argv[0]);
         return 1;
     }
-
+*/
     signal(SIGINT, sigint);
-    init_alsa_device(argv[1]);
-    net_init(argv[2],argv[3]);
+    //    init_alsa_device(argv[1]);
+    //net_init(argv[2],argv[3]);
     init_processing(); //will populate size of send_array in config_array.h
 
     send_data = (char*) calloc(NUM_TRIGGERS*size_of_sendarray, sizeof(char));
     temp_buffer = (uint16_t*)calloc(2*DATA_BUFFER_SIZE, sizeof(signed short));
+    dump_buffer = (char*)calloc(2*DATA_BUFFER_SIZE, sizeof(signed short));
     trigger = (float*) calloc(DATA_BUFFER_SIZE, sizeof(float));
     response = (float*) calloc(DATA_BUFFER_SIZE, sizeof(float));
-    int i;
+    int i, j, count;
+
+    count = 0;
 
     for(;;) {
-
+      /* read from the audio device and store in 2 float arrays*/
+      init_alsa_device(argv[1]);
         read_alsa_data((char*)temp_buffer, DATA_BUFFER_SIZE);
         s16_to_float_array(trigger, DATA_BUFFER_SIZE, 0, 1, temp_buffer);
         s16_to_float_array(response, DATA_BUFFER_SIZE, 1, 1, temp_buffer);
 
+	for ( i = 0; i <4*DATA_BUFFER_SIZE; i++)
+	  printf("%X\n", temp_buffer[i]);
 
         /* send_data[0][0] converts 2-d array to char*, pointing to first value*/
         process_radar_data(send_data, trigger,response, DATA_BUFFER_SIZE);
-        for (i = 0; i < NUM_TRIGGERS; i = i + 8){
-            //messagize_and_send(&send_data[NUM_TRIGGERS*i], size_of_sendarray);
-	// TODO: Update this to trim out "peaks"	
-	//	net_send_data((uint8_t*) &send_data[NUM_TRIGGERS*i], size_of_sendarray);
+
+	/* send small carrier messages per row, if greater than a threshold, 
+           send the index that indicates an object is there              */
+        for (i = 0; i < NUM_TRIGGERS; i = i + 1){
+	  printf("%d: ", count);
+	  for (j = 0; j < size_of_sendarray; j++){
+	    if (send_data[NUM_TRIGGERS*i +j] >= POWER_CUTOFF) {
+	      // printf("%d ", send_data[NUM_TRIGGERS*i +j]);
+	      //	      net_send_data(count,j);
+	    }
+	  }
+	  printf("\n");
+	  count++;
         }
+	deinit_alsa();
     }
 
 
