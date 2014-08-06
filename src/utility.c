b8f9b50f7fe9731b58e8556b0a1e401d57c26208
Eric Richter <erichter@srcinc.com>
2014-08-06 21:38:54 +0000

    Fixed uninitialized j

--- src/utility.c
+++ src/utility.c
@@ -3,7 +3,7 @@
 // Copies values from buffer to target (which must be size "size"), starting at the buffer offset, and incrementing every stride
 void s16_to_float_array(float * target, size_t size, int offset, int stride, uint16_t * buffer)
 {
-	int i,j;
+	int i,j = 0;
 
 	for (i = offset; j < size; j++,i+=stride) {
 		target[j] = (float) buffer[i];
