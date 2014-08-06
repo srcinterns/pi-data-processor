e552a465a9149901a25214bb8afaee3dd6741247
richteer <richteer@clarkson.edu>
2014-08-06 21:58:45 +0000

    Fixed problem with all data being positive (yay unsigned)

--- src/utility.c
+++ src/utility.c
@@ -1,7 +1,7 @@
 #include "utility.h"
 
 // Copies values from buffer to target (which must be size "size"), starting at the buffer offset, and incrementing every stride
-void s16_to_float_array(float * target, size_t size, int offset, int stride, uint16_t * buffer)
+void s16_to_float_array(float * target, size_t size, int offset, int stride, int16_t * buffer)
 {
 	int i,j = 0;
 
